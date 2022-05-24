b55138a50c2d55a755b7187ead98003cbf0501df
Akasha Peppermint <>
2022-05-25 03:27:40 +1000

    Rewrite so the bindings can support declarations:
    Previously this code snippet would fail:
    (if-let* ((a :foo) (b :bar))
      (declare (type symbol a b))
      t
      nil)
    
    as the declarations were treated as code bodies. This commit fixes that, as well as allows this example to work:
    
    (labels ((c () (print a) (print b)))
      (if-let* ((a :foo) (b (eq 1 1)))
        (declare (special a))
        (declare (special b))
        (declare (type symbol a b))
        (progn
          (print "true")
          (c))
        (progn
          (print "false")
          (c))))
    
    Note the x-let stuff from alexandria seems to suffer from this too.

--- if-letstar.lisp
+++ if-letstar.lisp
@@ -25,17 +25,17 @@ previously bound by the IF-LET*.
 
 Execution of IF-LET* causes the form BODY-2 to evaluate if any INITIAL-FORM evaluates to NIL.
 If all INITIAL-FORMs evaluate to true, then the form BODY-1 is executed."
-  (destructuring-bind (body-1 body-2) bodies
-    (let* ((binding-list (if (and (consp bindings) (symbolp (car bindings)))
-                             (list bindings)
-                             bindings))
-           (variables (mapcar #'car binding-list)))
-      `(let ,variables
-         (if (block nil
-               ,@(loop for b in binding-list
-                       for v in variables
-                       collect `(setq ,v (let (,b) ,v))
-                       collect `(unless ,v (return nil)))
-               t)
-             ,body-1
-             ,body-2)))))
+  (let* ((binding-list (if (and (consp bindings) (symbolp (car bindings)))
+                           (list bindings)
+                           bindings))
+         (variables (mapcar #'car binding-list)))
+    `(let ,variables
+       ;; the butlast and last calls allows this to insert declarations.
+       ,@(append (butlast bodies 2)
+                 `((if (and
+                        ,@(loop for b in binding-list
+                                for v in variables
+                                ;; the let is here simply to use the malformed let binding error
+                                ;; of the host implementation
+                                collect `(setq ,v (let (,b) ,v))))
+                       ,@(last bodies 2)))))))
