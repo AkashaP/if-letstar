963adab385c1d42cac970a19f1470814725419f3
Akasha Peppermint <>
2022-05-25 08:34:50 +1000

    Workarounds for last commit:
    Discovered painfully that shortcutted and statements seem to be broken or not actually a thing in CL or something, so that whole 'gate of setqs' thing i had earlier doesn't work (which i was taught would work in University).
    This happened when i did a drop-in replacement of these functions to an existing project that used them extensively from alexandria.
    The debugger stopped at a crash midway through x-let* varients and reported that the earlier bindings were nil and should have been stopped by the AND statement.
    This probably means the and statement in SBCL evaluates its arguments in parallel or something.
    So now instead i have a tower of setqs, and that hasn't seemingly had any problem yet.

--- if-letstar.lisp
+++ if-letstar.lisp
@@ -8,43 +8,6 @@
 
 (in-package :if-letstar)
 
-(defmacro if-let* (bindings &body bodies)
-  "Creates new symbol bindings, and conditionally executes either the second-last or last form of BODIES.
-
-BINDINGS must be either single binding of the form:
-
- (symbol initial-form)
-
-or a list of bindings of the form:
-
- ((symbol-1 initial-form-1)
-  (symbol-2 initial-form-2)
-  ...
-  (symbol-n initial-form-n))
-
-Each INITIAL-FORM is executed in turn, and the variable bound to the
-corresponding value. INITIAL-FORM expressions can refer to symbols
-previously bound by the IF-LET*.
-
-Any declarations can come below the bindings form, before the start of any significant code in BODIES.
-
-Execution of IF-LET* causes the last form of BODIES to evaluate if any INITIAL-FORM evaluates to NIL.
-If all INITIAL-FORMs evaluate to true, then the second-last form of BODIES is executed."
-  (let* ((binding-list (if (and (consp bindings) (symbolp (car bindings)))
-                           (list bindings)
-                           bindings))
-         (variables (mapcar #'car binding-list)))
-    `(let ,variables
-       ,@(remove nil ; don't leave random floating nils around that nil don't do nil nil anything
-                 `(,@(butlast bodies 2) ;insert declarations
-                   (if (and
-                        ,@(loop for b in binding-list
-                                for v in variables
-                                ;; the let is here simply to use the malformed let binding error
-                                ;; of the host implementation
-                                collect `(setq ,v (let (,b) ,v))))
-                       ,@(last bodies 2)))))))
-
 (defmacro if-let (bindings &body bodies)
   "Creates new symbol bindings, and conditionally executes either
 either the second-last or last form of BODIES. the last form defaults to NIL.
@@ -143,17 +106,69 @@ PROGN."
                            (list bindings)
                            bindings))
          (variables (mapcar #'car binding-list)))
-    `(let* ,binding-list
-       ,@(remove nil
-                 ;; try and retain declarations.
-                 ;; if something doesn't work, try a locally-declaim or something wrapping the block instead
-                 (loop for forms on body
-                       if (and (listp forms)
-                               (listp (car forms))
-                               (or (eq 'declare (caar forms))
-                                   (eq 'locally (caar forms))))
-                         collect (car forms)
-                       else collect `(when (and ,@variables)
-                                       ,@forms)
-                            and do (loop-finish))))))
+    `(let ,variables
+       ;; try and retain declarations.
+       ;; if something doesn't work, try a locally-declaim or something wrapping the block instead
+       ,@(loop for forms on body
+               while (and (listp forms)
+                          (listp (car forms))
+                          (or (eq 'declare (caar forms))
+                              (eq 'locally (caar forms))))
+               if (car forms) 
+                 collect (car forms) into result
+               finally (return `(,@result ,(if binding-list
+                                               (labels ((bind (b)
+                                                          `(when (setq ,(caar b) 
+                                                                       ;; the let is here simply to use the malformed let binding error
+                                                                       ;; of the host implementation
+                                                                       (let (,(car b)) ,(caar b)))
+                                                             ,@(if (cdr b)
+                                                                   `(,(bind (cdr b)))
+                                                                   forms))))
+                                                 (bind binding-list)))))))))
 
+
+(defmacro if-let* (bindings &body bodies)
+  "Creates new symbol bindings, and conditionally executes either the second-last or last form of BODIES.
+
+BINDINGS must be either single binding of the form:
+
+ (symbol initial-form)
+
+or a list of bindings of the form:
+
+ ((symbol-1 initial-form-1)
+  (symbol-2 initial-form-2)
+  ...
+  (symbol-n initial-form-n))
+
+Each INITIAL-FORM is executed in turn, and the variable bound to the
+corresponding value. INITIAL-FORM expressions can refer to symbols
+previously bound by the IF-LET*.
+
+Any declarations can come below the bindings form, before the start of any significant code in BODIES.
+
+Execution of IF-LET* causes the last form of BODIES to evaluate if any INITIAL-FORM evaluates to NIL.
+If all INITIAL-FORMs evaluate to true, then the second-last form of BODIES is executed."
+  (let* ((binding-list (if (and (consp bindings) (symbolp (car bindings)))
+                           (list bindings)
+                           bindings))
+         (variables (mapcar #'car binding-list)))
+    `(let ,variables
+       ,@(loop for forms on bodies
+               while (and (listp forms)
+                          (listp (car forms))
+                          (or (eq 'declare (caar forms))
+                              (eq 'locally (caar forms))))
+               if (car forms) 
+                 collect (car forms) into result
+               finally (return `(,@result (if ,(if binding-list
+                                                   (labels ((bind (b)
+                                                              `(when (setq ,(caar b) 
+                                                                           ;; the let is here simply to use the malformed let binding error
+                                                                           ;; of the host implementation
+                                                                           (let (,(car b)) ,(caar b)))
+                                                                 ,@(if (cdr b)
+                                                                       `(,(bind (cdr b)))))))
+                                                     (bind binding-list)))
+                                              ,@forms)))))))
